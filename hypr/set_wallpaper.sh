ac8ae6d885cf1281297262fa51c52f4a6606850a
alvin <alvinjian@gmail.com>
2025-06-07 23:22:45 -0400

    hyprland config and wallpaper updates

--- hypr/set_wallpaper.sh
+++ hypr/set_wallpaper.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 swww-daemon &
 sleep 0.5
-sww img ~/Pictures/wallpapers/wall.png --transition-type grow --transition-pos 0.5,0.5 --transition-fps 60 --transition-duration 2
+swww img ~/Pictures/wallpapers/fcmtawi16a3f1.jpeg --transition-type grow --transition-pos 0.5,0.5 --transition-fps 60 --transition-duration 2
